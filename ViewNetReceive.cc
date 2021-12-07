d60279a4f81b1be562a21801c67d92956606ff2b
Jorge Cabrera <alu0101351773@ull.edu.es>
2021-12-07 19:31:40 +0000

    Archivos de envío creados

---
+++ ViewNetReceive.cc
@@ -0,0 +1,10 @@
+#include "socket.h"
+#include <iostream>
+
+Message mensaje_enviado;
+
+int main(void) {
+    
+
+    return 0;
+}
---
+++ ViewNetSend.cc
@@ -0,0 +1,10 @@
+#include "socket.h"
+#include <iostream>
+
+Message mensaje_recibido;
+
+int main(void) {
+
+
+    return 0;
+}
